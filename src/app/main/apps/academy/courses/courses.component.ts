ad70107f4ff7c731ecc63e302c3bf95b3785f272
Josué Gabuardi <jgabuardi@beenverified.com>
2019-09-04 21:28:07 -0600

    add month data

--- src/app/main/apps/academy/courses/courses.component.ts
+++ src/app/main/apps/academy/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {fuseAnimations} from '@fuse/animations';
 import {typeCrimePerMonth} from './data/typeCrimePerMonth';
+import {totalCrimesPerAge} from './data/totalCrimesPerAge';
+import {CrimesService} from '../../../../services/crimes.service';
 
 @Component({
     selector: 'academy-courses',
@@ -10,7 +12,58 @@ import {typeCrimePerMonth} from './data/typeCrimePerMonth';
 })
 export class AcademyCoursesComponent {
 
-    total
+
+    constructor(private crimesService: CrimesService){
+
+        this.parseToLineChart(this.crimeTypePerMonth.data);
+
+        // this.crimesService.getData().subscribe(
+        //     value => {
+        //         console.log(value);
+        //         this.crimeTypePerMonth.data = value;
+        //     }
+        // );
+
+    }
+
+
+    private parseToLineChart(data): void {
+
+        const lineChart = {name: 'Crimes', series: []};
+
+        for (const month of data) {
+
+            let total = 0;
+
+            for (const crimeType of month.series) {
+
+                total += crimeType['value'];
+
+            }
+
+            lineChart.series.push({name: month['name'], value: total});
+
+        }
+
+        this.totalCrimePerAge.data = [lineChart];
+        console.log(lineChart);
+
+    }
+
+    // ------------------------------
+    // TOTAL CRIMES PER AGE
+    // ------------------------------
+    totalCrimePerAge = {
+        data: totalCrimesPerAge,
+        autoscale: true,
+        animations: true,
+        xAxis: {
+            show: true,
+            showLabel: true,
+            label: 'Tipo de Delito'
+        }
+    };
+    // ------------------------------
 
     // ------------------------------
     // CRIME TYPES PER MONTH OF AGE
@@ -22,6 +75,9 @@ export class AcademyCoursesComponent {
             show: true,
         },
         xAxis: {
+            show: true
+        },
+        yAxis: {
             show: true,
             showLabel: true,
             label: 'Tipo de Delito'
---
+++ src/app/services/crimes.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CrimesService {
+
+    URL = 'http://192.168.1.19:8000/delitos';
+
+    constructor(private http: HttpClient) {
+    }
+
+    getData(): Observable<any> {
+        return this.http.get(this.URL);
+    }
+
+}
