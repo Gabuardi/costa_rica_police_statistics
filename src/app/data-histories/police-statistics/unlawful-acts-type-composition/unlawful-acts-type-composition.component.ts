2356efa8722a1fcefc2bd9db97062e924fc9ee86
Josué Gabuardi <jgabuardi@beenverified.com>
2019-09-15 21:54:14 -0600

    add graphic

--- src/app/data-histories/police-statistics/unlawful-acts-type-composition/unlawful-acts-type-composition.component.ts
+++ src/app/data-histories/police-statistics/unlawful-acts-type-composition/unlawful-acts-type-composition.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UnlawfulActsService} from '../../../services/unlawful-acts.service';
+import {typeCrimePerMonth} from '../../../main/apps/academy/courses/data/typeCrimePerMonth';
 
 @Component({
-  selector: 'app-unlawful-acts-type-composition',
-  templateUrl: './unlawful-acts-type-composition.component.html',
-  styleUrls: ['./unlawful-acts-type-composition.component.scss']
+    selector: 'app-unlawful-acts-type-composition',
+    templateUrl: './unlawful-acts-type-composition.component.html',
+    styleUrls: ['./unlawful-acts-type-composition.component.scss']
 })
 export class UnlawfulActsTypeCompositionComponent implements OnInit {
 
-  constructor() { }
+    // ------------------------------
+    // PROPERTIES
+    // ------------------------------
+    crimeTypePerMonth = {
+        data: typeCrimePerMonth,
+        animations: true,
+        legend: {
+            show: true,
+        },
+        xAxis: {
+            show: true
+        },
+        yAxis: {
+            show: true,
+            showLabel: true,
+            label: 'Tipo de Delito'
+        }
+    };
 
-  ngOnInit() {
-  }
+    // ------------------------------
 
-}
+    constructor(private unlawfulActsService: UnlawfulActsService) {
+    }
+
+    ngOnInit() {
+        this.unlawfulActsService.getPorcentualData().subscribe(
+            value => {
+                this.crimeTypePerMonth.data = value;
+            }
+        );
+    }
+
+} // COMPONENT ENd
